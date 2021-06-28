4ab777ce1277c9236bf1ef814c4de1265c650700
Alisher Abduaxatov <alisherabduaxatov@Musobeks-MacBook-Pro.local>
2021-06-28 10:22:13 +0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace medAssisTantApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
